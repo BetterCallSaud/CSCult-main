0abe24ca682707c5da369bd737197d32dd13e880
Saud Hashmi <saud841@gmail.com>
2021-09-01 15:12:12 +0530

    Update Intro.js

--- src/components/Pages/DSA-Weekly/Module1/Submodules/Intro.js
+++ src/components/Pages/DSA-Weekly/Module1/Submodules/Intro.js
@@ -14,7 +14,7 @@ export default function Intro() {
                 </div><br/><br/>
                 <h2>Text Resources:</h2>
                 <div>
-                    <a target="_blank" className="anchor" href="https://www.youtube.com/watch?v=N89PN_uyelU&list=PLUcsbZa0qzu3yNzzAxgvSgRobdUUJvz7p&index=1">
+                    <a target="_blank" className="anchor" href="https://www.tutorialspoint.com/data_structures_algorithms/asymptotic_analysis.htm">
                         <h3>Asymptotic Analysis</h3>
                     </a>
                     <a target="_blank" className="anchor" href="https://www.tutorialspoint.com/data_structures_algorithms/algorithms_basics.htm">
