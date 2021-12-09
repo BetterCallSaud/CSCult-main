5b8cc448e5c869360baeff07ce53c4f902d10467
Mradul Gupta <93073227+mradul-gpt@users.noreply.github.com>
2021-12-09 10:14:19 +0530

    Added Navigation Bar
    removed old piece of code and created new navigation bar with toggling features.

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -5,30 +5,33 @@ import { Link } from "react-router-dom";
 
 function Header() {
 	return (
-		<div className="header">
-			<Link to="/">
-				<div className="header__left">
-					<img src={logo} alt="CSCult's Logo" />
-					<span>CSCult</span>
+
+		<div className="container-fluid">
+			<nav className="navbar navbar-expand-lg navbar-light">
+      			<a href="" className="navbar-brand"><img src={logo} alt="CSCult's Logo" className="nav-logo"/> CSCult</a>
+      			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          			<span className="navbar-toggler-icon"></span>
+     	 		</button>
+      			<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          			<ul className="navbar-nav ms-auto">
+              			<li className="nav-item">
+                  			<a href="/#about" className="nav-link">About</a>
+              			</li>
+              			<li className="nav-item">
+                  			<a href="/#community" className="nav-link">Community</a>
+              			</li>
+              			<li className="nav-item">
+                  			<a href="/#events" className="nav-link">Events</a>
+              			</li>
+						  <li className="nav-item">
+                  			<a href="/#products" className="nav-link">Products</a>
+              			</li>
+						<li className="nav-item">
+                  			<a href="/team" className="nav-link">Team</a>
+              			</li>
+        			</ul>
 				</div>
-			</Link>
-			<div className="header__right">
-				<a href="/#about">
-					<div className="header__option">About</div>
-				</a>
-				<a href="/#community">
-					<div className="header__option">Community</div>
-				</a>
-				<a href="/#events">
-					<div className="header__option">Events</div>
-				</a>
-				<a href="/#products">
-					<div className="header__option">Products</div>
-				</a>
-				<Link to="/team">
-					<div className="header__option">Team</div>
-				</Link>
-			</div>
+    		</nav>
 		</div>
 	);
 }
